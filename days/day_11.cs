a5b572d9c8b6ad53fb2099d02f29cb5ab28bdd54
Ross Overstreet <ro@overstreet.me>
2023-12-12 12:49:48 -0600

    Refactor & finish day_11

--- days/day_11.cs
+++ days/day_11.cs
@@ -1,36 +1,31 @@
 using System.Diagnostics;
-
 namespace advent_of_code_2023;
 class day_11: _base {  
   public static void run () 
   {  
-    var data = get_data_test();
+    var data = get_data_live();
     var first = calc_part_1(data);
     var second = calc_part_2(data);
 
     Console.WriteLine($"{nameof(day_11)}\n\t1st part:\t{first}\n\t2nd part:\t{second}");
   }
 
-  //        1x would be 374 
-  //       10x would be 1030
-  //      100x would be 8410
-  //    10000x would be ?   
-  //   100000x would be ?   
-  //  1000000x would be 9723824 
-  public static int calc_part_1(string data) {
+  public static long calc_part_1(string data) {
+    return calculate(data, expand_factor: 1);
+  }
 
+  public static long calc_part_2(string data) {
+    return calculate(data, expand_factor: 999999);
+  }
+
+  public static long calculate(string data, int expand_factor) {
     var matrix = parse_string_to_matrix(data);
     var empty_rows = get_empty_rows(matrix);
     var empty_cols = get_empty_columns(matrix);
-    var new_matrix = expand_matrix(matrix, empty_rows, empty_cols, expand_factor: 99999);
-    var number_positions = assign_numbers(new_matrix);
+    var number_positions = assign_numbers_virtually(matrix, empty_rows, empty_cols, expand_factor);
 
     return get_pairs(Enumerable.Range(1, number_positions.Count).ToArray())
       .Sum(x => calc_distance_between_numbers(x.Key, x.Value, number_positions));;
-  }
-
-  public static int calc_part_2(string data) {
-    return default(int);
   }
 
   /// <summary>
@@ -41,9 +36,6 @@ class day_11: _base {
     for (int i = 0; i <= matrix.GetUpperBound(0); i++) {
       var all_cols_empty = true;
       for (int j = 0; j <= matrix.GetUpperBound(1); j++) {
-        if (matrix[i, j] == default(char)) {
-          Console.WriteLine("default char");
-        }
         if (matrix[i, j] != '.' ) all_cols_empty = false; 
       }
       if (all_cols_empty) empty_rows.Add(i);
@@ -69,7 +61,7 @@ class day_11: _base {
   /// <summary>
   /// find each pair of numbers (a & b) in the matrix and calculate the distance between them
   /// </summary>
-  private static int calc_distance_between_numbers (int a, int b, Dictionary<int, Tuple<int,int>> positions) {
+  private static long calc_distance_between_numbers (int a, int b, Dictionary<int, Tuple<int,int>> positions) {
     Debug.Assert(positions.ContainsKey(a), $"number_positions should contain {a}");
     Debug.Assert(positions.ContainsKey(b), $"number_positions should contain {b}");
     
@@ -102,41 +94,12 @@ class day_11: _base {
   }
 
   /// <summary>
-  /// takes an existing matrix and expands it to include the requested empty rows and columns
-  /// the empty rows and columns are added in the middle of the matrix as they are found  
-  /// </summary>
-  private static char[,] expand_matrix(char[,] matrix, List<int> empty_rows, List<int> empty_cols, int expand_factor = 1) {
-    var new_matrix = new char[
-      matrix.GetUpperBound(0) + ((empty_rows.Count * expand_factor) + 1), 
-      matrix.GetUpperBound(1) + ((empty_cols.Count * expand_factor) + 1)];
-
-    var row_offset = 0;
-    for (int r = 0; r <= matrix.GetUpperBound(0); r++) {
-      if (empty_rows.Contains(r)) row_offset++;
-
-      var col_offset = 0;
-      for (int c = 0; c <= matrix.GetUpperBound(1); c++) {
-        if (empty_cols.Contains(c)) col_offset++;
-        new_matrix[r + (row_offset * expand_factor), c + (col_offset * expand_factor)] = matrix[r, c];
-      }
-    }
-
-    for (int r = 0; r <= new_matrix.GetUpperBound(0); r++) {
-      for (int c = 0; c <= new_matrix.GetUpperBound(1); c++) {
-        if (new_matrix[r, c] == default(char)) new_matrix[r, c] = '.';
-      }
-    }
-
-    return new_matrix;
-  }
-
-  /// <summary>
   /// for an array of numbers, returns a list of all possible pairs
   /// </summary>
   private static List<KeyValuePair<int, int>> get_pairs(int[] nums) {
     var pairs = new List<KeyValuePair<int, int>>();
-    for (int i = 0; i < nums.Length; i++) {
-      for (int j = i + 1; j < nums.Length; j++) {
+    for (long i = 0; i < nums.Length; i++) {
+      for (long j = i + 1; j < nums.Length; j++) {
         pairs.Add(new KeyValuePair<int,int>(nums[i], nums[j]));
       }
     }
@@ -146,34 +109,23 @@ class day_11: _base {
   /// <summary>
   /// finds the coordinates of all # symbols in the matrix, numbers them sequentially starting at 1, and returns the x, y coordinates for each
   /// </summary>
-  private static Dictionary<int, Tuple<int,int>> assign_numbers(char[,] matrix) {
+  private static Dictionary<int, Tuple<int,int>> assign_numbers_virtually(char[,] matrix, List<int> empty_rows, List<int> empty_cols, int expand_factor) {
     var numbers = new Dictionary<int, Tuple<int,int>>();
     var counter = 1;
-    for (int i = 0; i <= matrix.GetUpperBound(0); i++) {
-      for (int j = 0; j <= matrix.GetUpperBound(1); j++) {
-        if (matrix[i, j] == '#') {
-          numbers.Add(counter, new Tuple<int, int>(i, j));
+
+    var row_offset = 0;
+    for (int r = 0; r <= matrix.GetUpperBound(0); r++) {
+      if (empty_rows.Contains(r)) row_offset++;
+
+      var col_offset = 0;
+      for (int c = 0; c <= matrix.GetUpperBound(1); c++) {
+        if (empty_cols.Contains(c)) col_offset++;
+        if (matrix[r, c] == '#') {
+          numbers.Add(counter, new Tuple<int, int>(r + (row_offset * expand_factor), c + (col_offset * expand_factor)));
           counter++;
         }
       }
     }
     return numbers;
   }
-
-  /// <summary>
-  /// dumps the matrix to the console for debugging
-  /// unused but helpful for debugging when needed
-  /// </summary>
-  private static void print_matrix (char[,] matrix) {
-    var output = "";
-    // rows
-    for (int i = 0; i <= matrix.GetUpperBound(0); i++) {
-        // columns
-        for (int j = 0; j <= matrix.GetUpperBound(1); j++) {
-          output += matrix[i,j] + " ";
-        }
-        output += "\n";
-    }
-    Console.WriteLine(output);
-  } 
 }
